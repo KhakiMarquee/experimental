b0807b97b92374b5272d802968be604f33f38593
KhakiMarquee <o.p.adegoke@gmail.com>
2025-08-02 15:11:53 +0100

    Updating three

--- src/three_scene.js
+++ src/three_scene.js
@@ -1,4 +1,4 @@
-import * as THREE from three;
+import * as THREE from 'three';
 import { OrbitControls } from 'https://unpkg.com/three@0.160.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.160.1/examples/jsm/loaders/GLTFLoader.js';
 
