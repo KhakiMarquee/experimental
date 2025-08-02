0f6f26afb2200009802539a37903f1245ef01143
KhakiMarquee <o.p.adegoke@gmail.com>
2025-08-02 14:31:12 +0100

    Managing the large files

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { initUI } from './ui.js';
+import { initStrudelPlayer } from './strudel_player.js';
+import { initThreeScene } from './three_scene.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initUI();
+  initStrudelPlayer();
+});
+
