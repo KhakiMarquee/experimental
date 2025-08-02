85afab4269d429ae964aeca108b57206d4c96d32
KhakiMarquee <o.p.adegoke@gmail.com>
2025-08-02 15:50:00 +0100

    Preparing the Public modules for 3D view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { initUI } from './ui.js';
 import { initStrudelPlayer } from './strudel_player.js';
-import { initThreeScene } from './three_scene.js';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   initUI();
