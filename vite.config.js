4821d645d05296fd7280b611b3af7293268e8a29
KhakiMarquee <o.p.adegoke@gmail.com>
2025-09-11 21:37:02 +0100

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -23,14 +23,14 @@ export default {
         team: resolve(__dirname, 'pages/team.html'),
         error: resolve(__dirname, 'pages/404.html')
       },
-      external: ['p5.sound'], // mark p5.sound external so it’s not bundled
+      external: ['p5.sound', 'js-image-compressor'], // mark p5.sound external so it’s not bundled
       
       output: {
         manualChunks: undefined,
       },
     },
     commonjsOptions: {
-      include: [/node_modules/], // ensure CJS modules are handled
+      include: [/node_modules/, /p5.sound/, /js-image-compressor/], // ensure CJS modules are handled
     },
   },
   server: {
